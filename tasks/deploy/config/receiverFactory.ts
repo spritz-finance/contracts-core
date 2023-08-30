298deda3e151d4c9c2fa450e775c814b06ae1f5c
Eliyahu Gusovsky <eliyahud@gmail.com>
2023-08-30 13:23:19 +0300

    controller per env

--- tasks/deploy/config/receiverFactory.ts
+++ tasks/deploy/config/receiverFactory.ts
@@ -1,11 +1,13 @@
 import { getSpritzPayContractConfig } from "./spritzPay";
 
-const CONTROLLER_ADDRESS = "0x79f02cB9C54c3C07D4f6510910a9849Fa8DdA0c1";
+const CONTROLLERS: Record<string, string> = {
+  staging: "0xD6BE79a6A72A7d9cded331D91b479804AbA663dc",
+  production: "0x79f02cB9C54c3C07D4f6510910a9849Fa8DdA0c1",
+};
 
 export const getReceiverFactoryContractConfig = (env: string, network: string) => {
   const spritzPayConfig = getSpritzPayContractConfig(env, network);
-
   return {
-    args: [CONTROLLER_ADDRESS, spritzPayConfig.proxy],
+    args: [CONTROLLERS[env], spritzPayConfig.proxy],
   };
 };
